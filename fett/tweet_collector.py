026dfff5eb651f9f5e14a9f3d1c31ff0f10888a6
Aru Sahni <arusahni@gmail.com>
2016-05-28 14:02:40 -0400

    Quick and dirty sample data colletor.

---
+++ fett/tweet_collector.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""Collect tweets from Twitter"""
+
+import json
+import time
+
+import tweepy
+
+import secrets
+
+auth = tweepy.OAuthHandler(secrets.CONSUMER_KEY, secrets.CONSUMER_SECRET)
+auth.set_access_token(secrets.ACCESS_TOKEN, secrets.ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+TWEETS = []
+MAX_TWEETS = 1000
+
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(15 * 60)
+
+for i, tweet in enumerate(limit_handled(tweepy.Cursor(api.list_timeline, "IAmAru", "food-trucks").items())):
+    print("Getting tweet {}".format(i))
+    TWEETS.append(tweet)
+    if len(TWEETS) >= MAX_TWEETS:
+        break
+
+if TWEETS:
+    with open("tweets.json", "w") as outfile:
+        json.dump([tweet._json for tweet in TWEETS], outfile)
