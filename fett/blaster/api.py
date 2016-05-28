1d4e21107a6d2de9c5c45ce5f0c1894f65867fd6
Kevin Dwyer <dwyer@thehumangeo.com>
2016-05-28 14:19:31 -0400

    Some marshmallowing

--- fett/blaster/api.py
+++ fett/blaster/api.py
@@ -1,9 +1,21 @@
 import hug
+from marshmallow import Schema, fields
+from falcon import HTTP_400
+
+class TruckTweet(Schema):
+    handle = fields.Str()
+    text = fields.Str()
+    latitude = fields.Float()
+    longitude = fields.Float()
+    place = fields.Str()
 
 @hug.get('/analyze')
-def analyze(tweet: dict):
+def analyze(tweet: TruckTweet(), response):
     """Identifies location and time of food trucks from their tweets"""
+    if not tweet:
+        response.status = HTTP_400
+        return
+    print(tweet)
     return {}
-
 
 app = __hug_wsgi__
