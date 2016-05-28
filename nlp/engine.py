d5468a562b2e96195f8ea835cac81f193bb1ffb6
Justin Shelton <justin@thehumangeo.com>
2016-05-28 14:11:26 -0400

    Use the tweets.json file if available

--- nlp/engine.py
+++ nlp/engine.py
@@ -1,5 +1,7 @@
 """Engine to extract information from tweets."""
 import spacy
+import json
+import os
 
 nlp = spacy.load('en')
 
@@ -15,6 +17,10 @@ if __name__ == '__main__':
         u"The truck's on L between 19th & 20th (11-1:30)",
         u"The truck will be @NavyFederal 820 Follin Ln, Vienna, Va ⌚️11-2:00"
     ]
+    if os.path.exists('tweets.json'):
+        with open('tweets.json') as tweets_file:
+            tweets = json.load(tweets_file)
+        texts = [tweet['text'] for tweet in tweets]
     for i, doc in enumerate(nlp.pipe(texts, batch_size=50, n_threads=4)):
         print("{}: {}".format(doc, doc.is_parsed))
         for ent in extract_ents(doc):
