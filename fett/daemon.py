42e655624c7f3097c03613a7caac28bff8a9ee48
Aru Sahni <arusahni@gmail.com>
2016-05-29 10:15:31 -0400

    Daemon scaffolding.

---
+++ fett/daemon.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+"""The Fett daemon"""
+
+import argparse
+import asyncio
+import logging
+import sys
+import time
+from typing import Dict, Any
+
+from fett.twitter_collector import TwitterCollector
+
+logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
+
+logger = logging.getLogger("fett.daemon")
+
+TWEET_QUEUE = asyncio.Queue()
+
+def extract(tweet: Dict[str, Any]) -> str:
+    """Extract a tweet
+
+    :param tweet: The tweet to extract
+    :returns: The tweet ID
+
+    """
+    logging.debug("Extracting: %s", tweet["id"])
+    time.sleep(5)
+    return tweet["id"]
+
+async def enrich() -> None:
+    while True:
+        tweet = await TWEET_QUEUE.get()
+        logger.debug("Processing: %s", tweet["id"])
+        final = extract(tweet)
+        logger.debug("Done processing %s", final)
+        await asyncio.sleep(1)
+
+async def collect(twitter_collector, interval) -> None:
+    """Collect the tweets
+    :returns: TODO
+
+    """
+    loop = asyncio.get_event_loop()
+    while True:
+        for tweet in twitter_collector.collect():
+            logger.debug("Collecting: %s", tweet["id"])
+            TWEET_QUEUE.put_nowait(tweet)
+        await asyncio.sleep(interval)
+
+def start_collection(interval: int, since_id: str=None) -> None:
+    """Start the collection loop.
+
+    This runs indefinitely.
+
+    :param interval: The interval (in seconds) with which to run the collector
+    :param loop: The AsyncIO event loop onto which enrichment tasks should be    pushed
+    :param since_id: An optional ID to start collection after
+
+    """
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+    twitter_collector = TwitterCollector("FettAPI", "trucks")
+    loop.create_task(collect(twitter_collector, interval))
+    loop.create_task(enrich())
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
+
+def main():
+    """The main event loop
+
+    :returns: TODO
+
+    """
+    args = parse_args()
+    start_collection(args.interval)
+
+def parse_args():
+    """Parse the command line args
+    :returns: TODO
+
+    """
+    parser = argparse.ArgumentParser(description="The Fett daemon", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-i", "--interval", dest="interval", type=int, default=3, help="The interval (in minutes)")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main()
---
+++ fett/twitter_collector.py
@@ -0,0 +1,46 @@
+"""Collects statuses from Twitter"""
+
+import json
+import time
+from typing import Any, Dict, Iterable
+
+import tweepy
+
+from fett import secrets
+
+def limit_handled(cursor: tweepy.Cursor):
+    """Wrap cursor access with rate limiting
+
+    :param cursor: The cursor to siphon
+    :returns: Cursor items
+
+    """
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(15 * 60)
+
+class TwitterCollector:
+    """Now it's time for things I found in Tweets"""
+
+    def __init__(self, account_name: str, source_list: str) -> None:
+        """Init!"""
+        self._auth = tweepy.OAuthHandler(secrets.CONSUMER_KEY, secrets.CONSUMER_SECRET)
+        self._auth.set_access_token(secrets.ACCESS_TOKEN, secrets.ACCESS_TOKEN_SECRET)
+        self._api = tweepy.API(self._auth)
+        self.account_name = account_name
+        self.source_list = source_list
+
+    def collect(self, since_id: str=None) -> Iterable[Dict[str, Any]]:
+        """Collect tweets
+
+        :param since_id: TODO
+        :returns: TODO
+
+        """
+        data = json.load(open("tweets-5.json", "r"))
+        yield from data
+        # for page in limit_handled(tweepy.Cursor(self._api.list_timeline, self.account_name,
+        #                                         self.source_list).pages(1)):
+        #     yield from page
